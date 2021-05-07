3bd5795bb830fd314c1be958bae43b7b6a615651
DelfinaMolter <delfi.molter@gmail.com>
2021-05-07 08:48:28 -0300

    carga inicial

---
+++ app.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require ('express');
+const app =express ();
+
+app.listen(3000, ()=> console.log('Servidor corriendo en http://localhost:3000'));
+app.use(express.static(path.resolve(__dirname,'./public')));
+app.get('/', (req,res)=>res.sendFile(path.resolve(__dirname,'.views/home.html')))
