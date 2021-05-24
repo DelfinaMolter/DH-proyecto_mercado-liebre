49e655889f33c98a0522483581bbce313b023c16
DelfinaMolter <delfi.molter@gmail.com>
2021-05-23 22:11:24 -0300

    agrego rutas en el app.js

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const app =express ();
 app.listen(3000, ()=> console.log('Servidor corriendo en http://localhost:3000'));
 app.use(express.static(path.resolve(__dirname,'./public')));
 app.get('/', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
+app.get('/register', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get('/login', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/login.html')))
