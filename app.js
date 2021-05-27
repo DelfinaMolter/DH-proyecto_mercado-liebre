f63203833b8de34041a1a0c9ccd23fc4a5a6d166
DelfinaMolter <delfi.molter@gmail.com>
2021-05-27 20:44:35 -0300

    agregamos el app.js la coneccion a diferentes puertos

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require ('express');
 const app =express ();
 
-app.listen(3000, ()=> console.log('Servidor corriendo en http://localhost:3000'));
+app.set("port",process.env.PORT || 3000);
+app.listen(app.get("port"), ()=> console.log('Servidor corriendo en http://localhost:'+app.get("port")));
 app.use(express.static(path.resolve(__dirname,'./public')));
 app.get('/', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
 app.get('/register', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/register.html')))
