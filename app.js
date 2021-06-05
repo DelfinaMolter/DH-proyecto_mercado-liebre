8da87f51fb7182ac9820faed813d505aa987dfd5
DelfinaMolter <delfi.molter@gmail.com>
2021-06-05 19:31:07 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const path = require('path');
 const express = require ('express');
-const app =express ();
+const app = express ();
+
+const path = require('path');
 
 app.set("port",process.env.PORT || 3000);
+
 app.listen(app.get("port"), ()=> console.log('Servidor corriendo en http://localhost:'+app.get("port")));
+
 app.use(express.static(path.resolve(__dirname,'./public')));
-app.get('/', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
-app.get('/register', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/register.html')))
-app.get('/login', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/login.html')))
+
+app.get('/', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')) );
+app.get('/register', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/register.html')));
+app.get('/login', (req,res)=>res.sendFile(path.resolve(__dirname,'./views/login.html')));
